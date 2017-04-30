dd928d19d41fb62ca8013dbb3d432987809129d0
102941 <karolina.kolyszko@gmail.com>
2017-04-30 17:43:53 +0200

    funkcja z odejmowaniem

--- kalkulator.py
+++ kalkulator.py
@@ -4,4 +4,9 @@ def dodawanie_liczb():
     c = int(input())
     wynik = a + b + c
     return wynik
+def odejmowanie_liczb():
+    a = int(input())
+    b = int(input())
+    wynik = a - b
 print (dodawanie_liczb())
+
